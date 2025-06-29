329a2774c3f20f78c91ea87cc90e0bd08a8ccd97
sivapriyaa <sivapriyaa2006@gmail.com>
2025-06-29 17:33:33 +0530

    Fix: Add working homepage

--- pages/index.js
+++ pages/index.js
@@ -44,3 +44,15 @@ export default function Home({ posts = [] }) {
 
 
 }
+
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to the Blog!</h1>
+      <p>This is the homepage.</p>
+      <Link href="/create">Create a New Blog</Link>
+    </div>
+  );
+}
+
